e55e81f852341b5f6e15b8bde8769807a405d2ea
VenkatSaketh <vvsaketh24@gmail.com>
2023-05-13 16:11:28 +0530

    created project,side bar

---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+
+function SideBar() {
+  return (
+    <div className="sidebar">
+        <div className="nav">
+           <div>Home</div>
+           <div>Add Scenario</div>
+           <div>All Scenarios</div>
+           <div>Add Vehicle</div>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar;
